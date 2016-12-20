3663dd7616e67ef2a5cc5b4fb5d866f3698415eb
Bheema <bheemaray@techjini.com>
2016-12-20 20:53:37 +0530

    second build commit

---
+++ StockHawk-Bheema/app/build/generated/source/apt/androidTest/debug/com/udacity/stockhawk/BR.java
@@ -0,0 +1,5 @@
+package com.udacity.stockhawk;
+
+public class BR {
+        public static final int _all = 0;
+}
---
+++ StockHawk-Bheema/app/build/generated/source/dataBinding/androidTest/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="5adac921-ee30-404f-b6b7-d2c6bbfcbc01", modulePackage="com.udacity.stockhawk.test", sdkRoot="/Users/Bheema/Library/Android/sdk",layoutInfoDir="/Users/Bheema/Android-Projects/NanoDegree/StockHawkRepo/StockHawk-Bheema/app/build/intermediates/data-binding-info/androidTest/debug",exportClassListTo="",isLibrary=false,minSdk=21,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
