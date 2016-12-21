5cacf1515289c73a69c8e5ab74aaf2155126556a
Bheema <bheemaray@techjini.com>
2016-12-21 10:15:51 +0530

    removed build folder

--- StockHawk-Bheema/app/build/generated/source/apt/androidTest/debug/com/udacity/stockhawk/test/BR.java
+++
@@ -1,5 +0,0 @@
-package com.udacity.stockhawk.test;
-
-public class BR {
-        public static final int _all = 0;
-}
--- StockHawk-Bheema/app/build/generated/source/apt/debug/com/udacity/stockhawk/BR.java
+++
@@ -1,5 +0,0 @@
-package com.udacity.stockhawk;
-
-public class BR {
-        public static final int _all = 0;
-}
